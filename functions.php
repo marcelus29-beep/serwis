b35edf5afbf4d7b73224eed073f65cb9cdf2eb14
marcelus29-beep <marcelus29@gmail.com>
2026-07-18 19:10:32 +0200

    Create functions.php

---
+++ functions.php
@@ -0,0 +1,8 @@
+<?php
+defined( 'ABSPATH' ) || exit;
+
+// Include the theme framework.
+require_once __DIR__ . '/vendor/hivepress/hivetheme/hivetheme.php';
+
+// 3DPrintHub
+require_once __DIR__ . '/inc/3dprinthub.php';
