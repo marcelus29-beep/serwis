6c15cd3e6aca658a46aa762c4d7acd8a394202bd
marcelus29-beep <marcelus29@gmail.com>
2026-07-18 17:26:07 +0200

    Update listing-title.php

--- hivepress/listing/view/block/listing-title.php
+++ hivepress/listing/view/block/listing-title.php
@@ -1,6 +1,27 @@
 <?php
-defined('ABSPATH') || exit;
+/**
+ * 3DPrintHub
+ * Listing title
+ */
+
+defined( 'ABSPATH' ) || exit;
+
+$url = hivepress()->router->get_url(
+    'listing_view_page',
+    [
+        'listing_id' => $listing->get_id(),
+    ]
+);
+
+$title = $listing->get_title();
 ?>
-<a class="tdph-card-title" href="<?php echo esc_url(hivepress()->router->get_url('listing_view_page',['listing_id'=>$listing->get_id()])); ?>" title="<?php echo esc_attr($listing->get_title()); ?>">
-<?php echo esc_html($listing->get_title()); ?>
-</a>
+
+<h3 class="tdph-card-title-wrap">
+    <a
+        class="tdph-card-title"
+        href="<?php echo esc_url( $url ); ?>"
+        title="<?php echo esc_attr( $title ); ?>"
+    >
+        <?php echo esc_html( $title ); ?>
+    </a>
+</h3>
