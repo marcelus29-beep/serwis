232c7ca4c0201ae348e29ce7844181b776fbaa2a
marcelus29-beep <marcelus29@gmail.com>
2026-07-18 18:20:39 +0200

    Delete functions.php

--- functions.php
+++
@@ -1,8 +0,0 @@
-<?php
-defined( 'ABSPATH' ) || exit;
-
-// Include the theme framework.
-require_once __DIR__ . '/vendor/hivepress/hivetheme/hivetheme.php';
-
-// 3DPrintHub
-require_once __DIR__ . '/inc/3dprinthub.php';
