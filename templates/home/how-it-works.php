1fcb9d4a120fb4c29a523285c7ff85da84bdca59
marcelus29-beep <marcelus29@gmail.com>
2026-07-20 22:32:51 +0200

    Create how-it-works.php

---
+++ templates/home/how-it-works.php
@@ -0,0 +1,32 @@
+<?php
+/**
+ * Home - How it works
+ */
+?>
+<section class="tdph-how">
+    <div class="container">
+        <h2 class="tdph-how__title">Jak to działa?</h2>
+
+        <div class="tdph-how__grid">
+
+            <div class="tdph-how__item">
+                <div class="tdph-how__number">1</div>
+                <h3>Dodaj ogłoszenie</h3>
+                <p>Opublikuj model lub usługę wydruku 3D w kilka minut.</p>
+            </div>
+
+            <div class="tdph-how__item">
+                <div class="tdph-how__number">2</div>
+                <h3>Znajdź klienta</h3>
+                <p>Użytkownicy kontaktują się z Tobą bezpośrednio przez portal.</p>
+            </div>
+
+            <div class="tdph-how__item">
+                <div class="tdph-how__number">3</div>
+                <h3>Zrealizuj zamówienie</h3>
+                <p>Wydrukuj model i wyślij go kupującemu lub umów odbiór.</p>
+            </div>
+
+        </div>
+    </div>
+</section>
